92eb5fcfb95df559b0e65c71c24f60bd5623ad2d
Toby Mckenzie <toby@redwillowbrewery.com>
2015-03-23 10:12:07 +0000

    added webview route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Beerboard::Application.routes.draw do
   
   resources :beerlines
 
+  resources :webviews
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
